fa0f46917b915832c61460b625bf5cb1c79a5182
Raphael Jacob <r.jacob2002@gmail.com>
2016-07-14 21:11:08 +0200

    Making it able to show the current API-Key

--- packages/telegram/config.py
+++ packages/telegram/config.py
@@ -1,20 +1,23 @@
 #! /usr/bin/env python3
 import cgi, configparser
 arguments = cgi.FieldStorage()
-#print(arguments.value)
-key = ''
+key_argument = ''
 try:
-	key = arguments['key'].value
+	key_argument = arguments['key'].value
 except: pass
-if key != '':
-	#print('Hi')
-	#print(key)
+if key_argument != '':
 	Config = configparser.ConfigParser()
 	cfgfile = open("config",'w')
 	Config.add_section('config')
-	Config.set('config','key',key)
+	Config.set('config','key',key_argument)
 	Config.write(cfgfile)
 	cfgfile.close()
+key = ''
+try:
+	config = configparser.SafeConfigParser()
+	config.read('config')
+	key = config.get('config','key')
+except: pass
 print('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">')
 print('<html xmlns="http://www.w3.org/1999/xhtml">')
 print('<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />')
@@ -30,7 +33,13 @@ print('<tr><td align="center"><img src="icon.png"/></td></tr>')
 print('</table>')
 print('<h2>Actions</h2>')
 print('<table align="center">')
-print('<tr><td>To install Telegram visit <a href=https://telegram.org/>Telegram</a> and download the version for your device.<br>Then write this messages to @BotFather:<br>1. /newbot<br>2. *Chose a name for your bot*<br>3. *Chose a username ending with "bot"*<br>Now copy the Token into the Formular and press "Send"<br>')
+print('<tr><td>To install Telegram visit <a href=https://telegram.org/>Telegram</a> and download the version for your device.<br>Then write this messages to @BotFather:<br>1. /newbot<br>2. *Chose a name for your bot*<br>3. *Chose a username ending with "bot"*<br>Now copy the Token into the Formular and press "Send"')
+print('<tr><td>')
+if key != '':
+	print('<h3>Current API-Key: ' + key + '</h3>')
+else:
+	print('<h3>No API-Key set</h3>')
+print('</td></tr>')
 print('<tr><td>')
 print('<form>')
 print('<label for="key">API-Key: <input type="text" id="key" name="key"></label> <input type="submit" value="Send">')

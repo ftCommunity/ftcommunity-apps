7afd0d2cd9901ebb63d2b6c311122a90d64a03ec
Raphael Jacob <r.jacob2002@gmail.com>
2016-07-15 12:51:07 +0200

    Add language select in Settings APP

--- packages/settings/settings.py
+++ packages/settings/settings.py
@@ -1,33 +1,42 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-import sys
+import sys, configparser, os
 from TxtStyle import *
-
+global configpath
+configpath = '/media/sdcard/data/config.conf'
 class FtcGuiApplication(TxtApplication):
-    def __init__(self, args):
-        TxtApplication.__init__(self, args)
+	def __init__(self, args):
+		TxtApplication.__init__(self, args)
 
-        # create the empty main window
-        w = TxtWindow("Settings")
-        vbox = QVBoxLayout()
-
-        button_DE = QPushButton("Sprache: DE")
-        button_DE.clicked.connect(self.on_button_clicked_DE)
-        vbox.addWidget(button_DE)
-        button_EN = QPushButton("Language: EN")
-        button_EN.clicked.connect(self.on_button_clicked_EN)
-        vbox.addWidget(button_EN)
-
-        self.button_state = False
-
-        w.centralWidget.setLayout(vbox)
-        w.show()
-        self.exec_()
-    def on_button_clicked_DE(self):
-        print('DE')
-    def on_button_clicked_EN(self):
-        print('EN')
+		w = TxtWindow("Settings")
+		self.vbox = QVBoxLayout()
+		self.vbox.addStretch()
+		self.lbl = QLabel("Select your language:")
+		self.lbl.setObjectName("smalllabel")
+		self.lbl.setWordWrap(True)
+		self.lbl.setAlignment(Qt.AlignCenter)
+		self.vbox.addWidget(self.lbl)
+		btn_lang_EN = QPushButton('Language: EN')
+		btn_lang_EN.clicked.connect(self.on_button_clicked_EN)
+		self.vbox.addWidget(btn_lang_EN)
+		btn_lang_DE = QPushButton('Sprache: DE')
+		btn_lang_DE.clicked.connect(self.on_button_clicked_DE)
+		self.vbox.addWidget(btn_lang_DE)
+		w.centralWidget.setLayout(self.vbox)
+		w.show()
+		self.exec_()
+	def set_lang(self,lang):
+		Config = configparser.ConfigParser()
+		cfgfile = open(configpath,'w')
+		Config.add_section('general')
+		Config.set('general','language',lang)
+		Config.write(cfgfile)
+		cfgfile.close()
+	def on_button_clicked_DE(self):
+		self.set_lang('DE')
+	def on_button_clicked_EN(self):
+		self.set_lang('EN')
 
 if __name__ == "__main__":
-    FtcGuiApplication(sys.argv)
+	FtcGuiApplication(sys.argv)

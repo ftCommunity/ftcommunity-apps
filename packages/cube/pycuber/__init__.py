0cb21031dc00fdd898cfe3077f615158476c4431
Till Harbaum <till@harbaum.org>
2016-12-27 20:26:41 +0100

    Removed currently unsuppotred cube solver

--- packages/cube/pycuber/__init__.py
+++
@@ -1,8 +0,0 @@
-from .cube import Cube, Cubie, Centre, Corner, Edge, Square, Step, Formula
-from .helpers import fill_unknowns, array_to_cubies
-
-__all__ = ["Cube", "Cubie", "Centre", "Corner", "Edge", "Square", "Step", "Formula"]
-__title__ = "PyCuber"
-__version__ = "0.2.1"
-__author__ = "Adrian Liaw"
-__license__ = "MIT"

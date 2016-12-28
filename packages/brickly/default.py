0673f616c9f2990c0b5cb51a1d97ad566f533d13
Till Harbaum <till@harbaum.org>
2016-12-28 11:19:35 +0100

    Moved apps into new repository

--- packages/brickly/default.py
+++
@@ -1,8 +0,0 @@
-# highlightBlock('KD8.vwB-H@^)YNa0$*xC');
-print('Hallo TXT!')
-# highlightBlock('NtPjn#U9nUK4xc,WRVag');
-setOutput(0, (True))
-# highlightBlock('X{d7^ZM~^-(:FV;PGB*5');
-time.sleep(1)
-# highlightBlock('Ph`X:459g*:B3tf~hH}J');
-setOutput(0, (False))
